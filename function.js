686c1be26ef059925fb1e5caddb0bc36a7ed3b07
tntcvad <48907412+tntcvad@users.noreply.github.com>
2019-04-10 11:40:43 +0700

    hàm chuyển ki tu hoa thành thường và thường thành Hoa

--- function.js
+++ function.js
@@ -1,11 +1,28 @@
-function square (number)
-{
-  var result = number*number;
-  return result;
+function checkPrimes(n){
+
+  let flag = true;    //biến cờ hiệu;
+  if(n < 2){
+    flag = false;
+  }
+
+  else {
+    {
+      for(i = 2;i < Math.sqrt(n) ; i++)
+        if( n % i == 0) {
+          flag = false;
+          break;
+        }
+    }
+  }
+    //Kiểm tra biến cờ hiệu
+  if(flag == true){
+    document.write(n +"là số nguyên tố" + "<br/>");
+  }
+  else {
+    document.write(n +" không phải số nguyên tố" + "<br/>");
+  }
+
 }
-function caculate() {
-  var input = document.getElementById("input").value;
-  var result = square(input);
-  document.getElementById("result").innerHTML = result;
+
+
   
-}
