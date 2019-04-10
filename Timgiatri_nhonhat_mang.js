54696575f831dc843d74038660b71eddeabb9bc6
tntcvad <48907412+tntcvad@users.noreply.github.com>
2019-04-10 16:46:04 +0700

    xay dung ham tim gia tri nho nhat mang

---
+++ Timgiatri_nhonhat_mang.js
@@ -0,0 +1,10 @@
+function minArray(array) {
+    let min = array[0];
+    for(i=0;i<array.length;i++) {
+        if ( a = array[i] < min ) {
+            min = array[i]
+        }
+    }
+    return min;
+    
+}
